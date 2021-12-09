a863a89905301f168a2a6ef21ed99ae44fb0aea5
SajawalHassan <scientificninja1f12@gmail.com>
2021-12-09 15:44:48 +0500

    Allow other domains to access pi

--- index.js
+++ index.js
@@ -2,19 +2,25 @@ const express = require("express");
 const mongoose = require("mongoose");
 const postRoute = require("./routes/posts");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 require("dotenv/config");
 
+// Initializing app
 const app = express();
 
+// Middleware
+app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("We are on home page!");
 });
 
+// Saying all routes in "postRoute" should start with "/posts"
 app.use("/posts", postRoute);
 
+// Connection db
 mongoose.connect(process.env.DB_CONNECTION, () =>
   console.log("Connected to DB!")
 );
